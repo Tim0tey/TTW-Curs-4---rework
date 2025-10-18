6d3ba7399eada4f67984e2805e97b0bd7229fa51
Tim0tey <timotei.termure@emanuel.ro>
2025-10-18 19:13:14 +0300

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'TTW-Curs-4---rework',
   plugins: [
     vue(),
     vueDevTools(),
